e75c74a8fe3da4f5674648c8a7c7b092c865669d
Maximilien Tragarz Quintana <BladedGoose@gmail.com>
2025-08-26 14:55:31 -0600

    Change expander to container for state properties

--- frontend/app.py
+++ frontend/app.py
@@ -33,7 +33,7 @@ def main():
     meta = Load_Tables()
     st.caption(f"Tables loaded from: `{meta['loaded_from']}`")
 
-    with st.expander("State Properties"):
+    with st.container("State Properties"):
         
         st.session_state["mi_key"] = None
         st.markdown("<div style='text-align: center;'><h2 style='font-size:20px;'>You can choose to use Temperature, Pressure or both.<br> Please leave unused properties empty</h2></div>", unsafe_allow_html=True)
